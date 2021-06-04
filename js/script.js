fed3788e2392b0c7efdb76ff7642f68d69d20fdc
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-04 17:27:09 +0200

    check

--- js/script.js
+++ js/script.js
@@ -6,24 +6,28 @@ genera.addEventListener("click", function (){
     var eta = document.getElementById("eta").value;
     var costo = km * 0.21;
 
-    document.getElementById("nomePass").innerHTML = name;
-
-    if(eta == "minore") {
-        document.getElementById("offerta").innerHTML = "Sconto Minorenne";
-        document.getElementById("costo").innerHTML = (costo *= 0.8).toFixed(2) + "€";
-    } else if (eta == "over65") {
-        document.getElementById("offerta").innerHTML = "Sconto Over 65";
-        document.getElementById("costo").innerHTML = (costo *= 0.6).toFixed(2) + "€";
+    if (name.toUpperCase() == name.toLowerCase() || costo < 0 || costo == "") {
+        alert("Inserire i dati obbligatori correttamente");
     } else {
-        document.getElementById("offerta").innerHTML = "Prezzo Intero";
-        document.getElementById("costo").innerHTML = costo.toFixed(2) + "€";
+        document.getElementById("nomePass").innerHTML = name;
+
+        if(eta == "minore") {
+            document.getElementById("offerta").innerHTML = "Sconto Minorenne";
+            document.getElementById("costo").innerHTML = (costo *= 0.8).toFixed(2) + "€";
+        } else if (eta == "over65") {
+            document.getElementById("offerta").innerHTML = "Sconto Over 65";
+            document.getElementById("costo").innerHTML = (costo *= 0.6).toFixed(2) + "€";
+        } else {
+            document.getElementById("offerta").innerHTML = "Prezzo Intero";
+            document.getElementById("costo").innerHTML = costo.toFixed(2) + "€";
+        }
+
+        document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 9 + 1);
+
+        document.getElementById("codice").innerHTML = Math.floor(Math.random() * 10000 + 90000);
+
+        document.getElementById("viewTicket").className += " show";
     }
-
-    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 9 + 1);
-
-    document.getElementById("codice").innerHTML = Math.floor(Math.random() * 10000 + 90000);
-
-    document.getElementById("viewTicket").className += " show";
 })
 
 var annulla = document.getElementById("annulla");
